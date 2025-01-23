11b89b314ccffe7f6a89f825e230811f175fa84f
adib <adibakkari@gmail.com>
2025-01-23 18:36:15 -0500

    reverting to normal map

--- app/index.tsx
+++ app/index.tsx
@@ -1,46 +1,11 @@
 import React from 'react';
-import { Alert, StyleSheet, View } from 'react-native';
-import MapView, { Geojson, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import buildings from '@/assets/Buildings/BuildingsMarkers';
-import floorPlan from '@/assets/FloorPlan/floorplan.json';
-
-
-const SGW_BUILDING={
-  latitude: 45.497092, 
-  longitude: -73.578800, 
-  latitudeDelta: 0.05, 
-  longitudeDelta: 0.05,
-}
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        provider={PROVIDER_GOOGLE}
-        showsUserLocation
-        showsMyLocationButton
-        initialRegion={SGW_BUILDING}
-      >
-        {buildings.map((building, index) => (
-          <Marker
-            key={index}
-            coordinate={{
-              latitude: building.Latitude, // Ensure these are numbers
-              longitude: building.Longitude,
-            }}
-            title={building.BuildingName}
-            description={building.BuildingLongName}
-            // onPress={() => onMarkerSelected(building)}
-          />
-        ))}
-          <Geojson
-          geojson={floorPlan}
-          strokeColor="blue"
-          fillColor="rgba(0, 0, 255, 0.3)" 
-          strokeWidth={3} 
-        />
-      </MapView>
+      <MapView style={styles.map} />
     </View>
   );
 }
